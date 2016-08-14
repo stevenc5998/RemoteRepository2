bef2fe820c21fb44818bd8428462c5c642523a3d
Steven <Steven@game5518>
2016-08-15 00:01:50 +0800

    0815

--- 08_11/src/com/team/controller/TeamServlet.java
+++ 08_11/src/com/team/controller/TeamServlet.java
@@ -7,22 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/TeamServlet")
-public class TeamServlet extends HttpServlet {
+public class TeamServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-   
-    public TeamServlet() {
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TeamServlet()
+	{
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		doPost(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
 		req.setCharacterEncoding("UTF-8");
 
 	}
